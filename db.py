f012418e0868355f8ae86cf92f800e365f7efda1
kirillmark <121565477+kirillmark@users.noreply.github.com>
2024-07-05 11:35:05 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from random import choice
+from emoji import emojize
 from pymongo import MongoClient
 import settings
 
@@ -14,7 +17,18 @@ def get_or_create_user(db, effective_user, chat_id):
             "first_name": effective_user.first_name,
             "last_name": effective_user.last_name,
             "username": effective_user.username,
-            "chat_id": chat_id
+            "chat_id": chat_id,
+            "emoji": emojize(choice(settings.USER_EMOJI), use_aliases=True),
         }
         db.users.insert_one(user)
     return user
+
+
+def save_anketa(user_id, anketa_data):
+    user = db.users.find_one({"user_id": user_id})
+    anketa_data['created'] = datetime.now()
+    if not 'anketa' in user:
+        db.users.update_one(
+            {'_id': user['_id']},
+            {'$set': {'anketa': [anketa_data]}}
+        )
